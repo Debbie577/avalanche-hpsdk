7ad28e9e34e30e9045796c462ae18f85dc451b41
Debbie577 <131953166+Debbie577@users.noreply.github.com>
2024-06-23 14:24:46 +0000

    aphiacn

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "APHIA-LECUM"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "LECUM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "LEC"
 )
 
 var ID ids.ID
